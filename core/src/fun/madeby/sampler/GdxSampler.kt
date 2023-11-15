4a6ec6963d4eda5ceec70cd8496742f6ce341956
gra-m <grahamd483@gmail.com>
2023-11-15 12:34:25 +0100

    batch and img lateinit

--- core/src/fun/madeby/sampler/GdxSampler.kt
+++ core/src/fun/madeby/sampler/GdxSampler.kt
@@ -12,8 +12,13 @@ class GdxSampler : ApplicationAdapter() {
     // batch cannot be created until framework is up and running, hence use of Kotlin's late init initializer
     // without this (libGDX in Java) is capable of having batch initialised in time not to get the UnsatisfiedLinkError
     // in Kotlin, this cannot be initialized in time.
-    var batch: SpriteBatch = SpriteBatch()
-    var img: Texture = Texture("badlogic.jpg")
+    lateinit var batch: SpriteBatch
+    lateinit var img: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
 
     override fun render() {
         Gdx.gl.glClearColor(1.0f, 0.0f, 0.0f, 1.0f)
