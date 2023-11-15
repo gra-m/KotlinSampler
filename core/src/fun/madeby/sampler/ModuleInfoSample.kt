17573e4acd127d3719ef8b4bc02425b70ab2805e
gra-m <grahamd483@gmail.com>
2023-11-15 16:42:27 +0100

    The static modules utilized within LibGdx and when they become active: DesktopLauncherGdxModuleInfo.kt

---
+++ core/src/fun/madeby/sampler/ModuleInfoSample.kt
@@ -0,0 +1,27 @@
+//Created by Graham Duthie on 15/11/2023 12:11 -1h
+package `fun`.madeby.sampler
+import com.badlogic.gdx.Application
+import com.badlogic.gdx.Application.LOG_DEBUG
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.utils.Logger
+
+
+class ModuleInfoSample : ApplicationAdapter() {
+    companion object {
+        @JvmStatic
+        private val LOG = Logger(ModuleInfoSample::class.java.simpleName, LOG_DEBUG)
+    }
+
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+        LOG.debug("app = ${Gdx.app}")
+        LOG.debug("audio = ${Gdx.audio}")
+        LOG.debug("input = ${Gdx.input}")
+        LOG.debug("files = ${Gdx.files}")
+        LOG.debug("net = ${Gdx.net}")
+        LOG.debug("graphics = ${Gdx.graphics}")
+    }
+
+
+}
---
+++ desktop/src/fun/madeby/sampler/DesktopLauncherGdxModuleInfo.kt
@@ -0,0 +1,20 @@
+//Created by Graham Duthie on 15/11/2023 12:06 -1h
+package `fun`.madeby.sampler
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+fun main() {
+
+    println("app is null before constructor = ${Gdx.app}")
+    println("input as well = ${Gdx.input}")
+
+    Lwjgl3Application(ModuleInfoSample(), Lwjgl3ApplicationConfiguration())
+    
+    println("app = ${Gdx.app}")
+    println("input = ${Gdx.input}")
+    
+}
+
+
