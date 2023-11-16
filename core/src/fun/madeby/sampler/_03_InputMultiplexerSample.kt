84f74a0ad3a446f57cb58d5844d538ddfc287529
gra-m <grahamd483@gmail.com>
2023-11-16 12:37:15 +0100

    Input Multiplexer == add multiple InputProcessors Polling == @ fps == called in draw()/linked to render. Input Listening == more efficient extend InputProcessor

---
+++ core/src/fun/madeby/sampler/_03_InputMultiplexerSample.kt
@@ -0,0 +1,49 @@
+package `fun`.madeby.sampler
+
+import com.badlogic.gdx.*
+import `fun`.madeby.sampler.utils.logger
+
+class _03_InputMultiplexerSample : ApplicationAdapter() {
+    companion object {
+        @JvmStatic
+        private val LOG = logger<_03_InputMultiplexerSample>()
+    }
+
+
+
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+// true == stops chaining.
+        val inputProcessorOne = object : InputAdapter() {
+            override fun keyDown(keycode: Int): Boolean {
+                LOG.debug("KeyDownP1() keycode= $keycode")
+                // false propagates to next input processor in the multiplexer loop
+                return false
+            }
+            override fun keyUp(keycode: Int): Boolean {
+                LOG.debug("KeyUpP1() keycode= $keycode")
+                return false
+            }
+        }
+
+        val inputProcessorTwo = object :InputAdapter() {
+            override fun keyDown(keycode: Int): Boolean {
+                LOG.debug("KeyDownP2() keycode= $keycode")
+                return true
+            }
+            override fun keyUp(keycode: Int): Boolean {
+                LOG.debug("KeyUpP2() keycode= $keycode")
+                return false
+            }
+        }
+
+        /*val multiplexer = InputMultiplexer()
+        multiplexer.addProcessor(inputProcessorOne)
+        multiplexer.addProcessor(inputProcessorTwo)
+        Gdx.input.inputProcessor = multiplexer */
+        Gdx.input.inputProcessor = InputMultiplexer(inputProcessorOne, inputProcessorTwo)
+
+    }
+
+
+}
---
+++ desktop/src/fun/madeby/sampler/_03_DL_InputMultiplexer.kt
@@ -0,0 +1,14 @@
+//Created by Graham Duthie on 15/11/2023 12:06 -1h
+package `fun`.madeby.sampler
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setWindowSizeLimits(1080, 720, 1080, 720)
+
+    Lwjgl3Application(_03_InputMultiplexerSample(), config)
+}
+
+
